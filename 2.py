801abbe4609a7505e18186e3a551eb6793c4cde2
polefremova <82303858+polefremova@users.noreply.github.com>
2021-04-20 21:20:36 +0500

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,19 +1,11 @@
-t = int
-hour = int
-minute = int
-sec = int
-t = int(input("Приветствую, введи свое число в секундах: "))
-hour = t // 3600
-minute = (t - hour * 3600) // 60
-sec = t - (hour * 3600 + minute * 60)
-if minute < 10:
-    minute = str("0" + str(minute))
-else:
-    minute = int(minute)
-if sec < 10:
-    sec = str("0" + str(sec))
-else:
-    sec = int(sec)
-print ("Твое число в формате чч:мм:сс - %s : %s : %s" %(hour,minute,sec))
+# Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
+# имя, фамилия, год рождения, город проживания, email, телефон.
+# Функция должна принимать параметры как именованные аргументы.
+# Реализовать вывод данных о пользователе одной строкой.
 
 
+def profile(f_name, l_name, birthyear, city, email, phone):
+    print(f_name, l_name, birthyear, city, email, phone, end=" ")
+
+
+profile((str(input("Имя: "))), (str(input("Фамилия: "))), (int(input("Год рождения: "))), (str(input("Город проживания: "))), (str(input("email: "))), (int(input("Телефон: "))))
